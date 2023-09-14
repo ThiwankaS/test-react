b019e29bbb31adf553ff41d41af2a356117fa713
Thiwanka Somachandra <thivankas@outlook.com>
2023-09-14 21:06:38 +0300

    Adding styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 
+function MyInfo() {
+  return (
+    <div>
+      <h1 className='Heading'>Thiwanka Somachandra</h1>
+      <p className='TagLine'>I want to Learn and Master Full Stack Web Development</p>
+      <ul className='List'>
+        <li>Norway</li>
+        <li>Germany</li>
+        <li>Switzerland</li>
+      </ul>
+    </div>
+  )
+  }
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>Thiwanka</li>
-        <li>Kaushalya</li>
-        <li>Siheli</li>
-      </ul>
+      <MyInfo/>
     </div>
   );
 }
